739c77fe37a5eeaafca181ccba65c450c429f116
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-07-06 14:10:31 +1200

    Edit .js

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,11 @@
-const grid = document.querySelector("#grid");
+const body = document.querySelector("body");
+body.setAttribute("style", "display: flex; gap: 16px;");
 
-grid.setAttribute("style", "display: flex; gap: 16px;");
+const grid = document.querySelector("#grid");
+grid.setAttribute("style", "display: flex; gap: 0px;");
 
 for (let i = 0; i < 8; i++) {
     const div = document.createElement("div");
-    div.setAttribute("style", "background-color: green; padding: 16px; margin: 0;");
+    div.setAttribute("style", "background-color: green; padding: 32px; margin: 1px;");
     grid.appendChild(div);
 }
