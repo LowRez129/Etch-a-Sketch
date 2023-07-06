236461c6ede017db328a3bce26ac8882245e5fd3
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-07-06 14:03:13 +1200

    Edit .js

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,7 +4,6 @@ grid.setAttribute("style", "display: flex; gap: 16px;");
 
 for (let i = 0; i < 8; i++) {
     const div = document.createElement("div");
-    const div1 = document.createElement("div");
     div.setAttribute("style", "background-color: green; padding: 16px; margin: 0;");
     grid.appendChild(div);
 }
