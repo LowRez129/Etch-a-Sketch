9eadb7cb13ee9facb0fff02aff67ab1e0da4eef5
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-07-06 14:52:53 +1200

    Edit .js

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,7 @@
-let grid_size = {x: 8, y: 8};
+let grid_size = {x: 16, y: 16};
+let pixel_size = 8;
+let spacing = 1;
+let color = "green";
 
 
 const body = document.querySelector("body");
@@ -8,13 +11,18 @@ const grid = document.createElement("div");
 grid.setAttribute("style", "display: flex;");
 body.appendChild(grid);
 
+function draw() {
+    color = "black";
+}
+
 for (let i = 0; i < grid_size.x; i++) {
     const grid = document.createElement("div");
     grid.setAttribute("style", "display: flex; flex-direction: column;");
     body.appendChild(grid);
     for (let i = 0; i < grid_size.y; i++) {
         const div = document.createElement("div");
-        div.setAttribute("style", "background-color: green; padding: 32px; margin: 1px;");
+        div.addEventListener("click", draw);
+        div.setAttribute("style", `background-color: ${color}; padding: ${pixel_size}px; margin: ${spacing}px;`);
         grid.appendChild(div);
     }
 }
