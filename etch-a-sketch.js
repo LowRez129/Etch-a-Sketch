236c474c5aa2eb51a05acb123e30e670b880800b
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-07-06 13:31:15 +1200

    Edit .js

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,11 @@
 const body = document.querySelector("body");
-const div = document.createElement("div", );
+const div = document.createElement("div");
+const div1 = document.createElement("div");
 
 body.setAttribute("style", "display: flex;");
-
 div.setAttribute("style", "background-color: green; padding: 16px; margin: 0;");
-div.textContent = "TEST";
+div1.setAttribute("style", "background-color: blue; padding: 16px; margin: 0;");
+//div.textContent = "TEST";
 
 body.appendChild(div);
+body.appendChild(div1);
