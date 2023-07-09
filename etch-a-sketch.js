01bb41391d77fc1b97dbe47029cd4d987b3b1d1c
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-07-10 10:56:05 +1200

    Edit All

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,25 +4,37 @@ let pixel_size = 8;
 let spacing = 1;
 let color = "green";
 
-const body = document.querySelector("#grid-container");
-body.setAttribute("style", "display: flex;");
-
-const grid = document.createElement("div");
-grid.setAttribute("style", "display: flex;");
-body.appendChild(grid);
-
 function draw() {
-    this.style["background-color"] = "black";
-}
+    const body = document.querySelector("#grid-container");
+    body.setAttribute("style", "display: flex;");
 
-for (let i = 0; i < grid_size.x; i++) {
     const grid = document.createElement("div");
-    grid.setAttribute("style", "display: flex; flex-direction: column;");
+    grid.setAttribute("style", "display: flex;");
     body.appendChild(grid);
-    for (let i = 0; i < grid_size.y; i++) {
-        const div = document.createElement("div");
-        div.addEventListener("click", draw);
-        div.setAttribute("style", `background-color: ${color}; padding: ${pixel_size}px; margin: ${spacing}px;`);
-        grid.appendChild(div);
+
+    for (let i = 0; i < grid_size.x; i++) {
+        const grid = document.createElement("div");
+        grid.setAttribute("style", "display: flex; flex-direction: column;");
+        body.appendChild(grid);
+        for (let i = 0; i < grid_size.y; i++) {
+            const div = document.createElement("div");
+            div.addEventListener("click", function () {
+                this.style["background-color"] = "black";
+            });
+            div.setAttribute("style", `background-color: ${color}; padding: ${pixel_size}px; margin: ${spacing}px;`);
+            grid.appendChild(div);
+        }
     }
 }
+
+draw();
+
+function adjustGridSize() {
+    const slider = document.querySelector("#grid-slider");
+    slider.oninput = function () {
+        square_grid = this.value;
+        console.log(square_grid);
+    }
+}
+
+adjustGridSize();
